3ca474ba9881d50a2f30ddbbbbb8bb01ceec8e75
Harry Rogers Woollett <harry.rw96@gmail.com>
2023-02-08 17:51:24 +1100

    Validate login load

--- cypress/e2e/winning.cy.js
+++ cypress/e2e/winning.cy.js
@@ -1,6 +1,7 @@
 describe('Winning Automation Exercise', () => {
   it('Login, Filter, Add to cart & Validate ', () => {
     cy.visit('https://www.saucedemo.com/')
+    cy.url().should('include', 'https://www.saucedemo.com')
     
   })
 })
