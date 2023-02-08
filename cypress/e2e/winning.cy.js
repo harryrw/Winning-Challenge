2f6683faa985da650b00ed796ce174f3b7af7f0a
Harry Rogers Woollett <harry.rw96@gmail.com>
2023-02-08 18:08:10 +1100

    Login to site

--- cypress/e2e/winning.cy.js
+++ cypress/e2e/winning.cy.js
@@ -2,6 +2,7 @@ describe('Winning Automation Exercise', () => {
   it('Login, Filter, Add to cart & Validate ', () => {
     cy.visit('https://www.saucedemo.com/')
     cy.url().should('include', 'https://www.saucedemo.com')
-    
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce{enter}')
   })
 })
